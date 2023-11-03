8df54bace35541a37141803cc0a2d282c02ac775
tarek <tarek.ouachani01@gmail.com>
2023-11-03 14:16:25 +0100

    work with lombok s annotations

--- src/main/java/org/example/entities/Employee.java
+++ src/main/java/org/example/entities/Employee.java
@@ -1,8 +1,17 @@
 package org.example.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Employee {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,55 +23,4 @@ public class Employee {
         @ManyToOne
         private Department department;
 
-    public Employee(Long id, String name, double salary, Department department) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-        this.department = department;
-    }
-
-    public Employee() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", salary=" + salary +
-                ", department=" + department +
-                '}';
-    }
 }
